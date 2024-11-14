a59149fe7f3415364ffbb9076db16516003a6181
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-14 10:24:39 -0600

    Create BarGenerator.js

---
+++ BarGenerator.js
@@ -0,0 +1,23 @@
+class BarGenerator {
+   constructor(props) {
+      this.fullColor = props.fullColor ?? "\\c5"
+      this.emptyColor = props.emptyColor ?? "\\c0"
+      this.character = props.character ?? "="
+      this.barLength = props.barLength ?? 4
+   }
+
+   generate(quantity, maxQuantity) {
+      // Give us a percentage represented in integer number of bars
+      quantity = Math.min(quantity, maxQuantity)
+      const percentageFull = Math.max(Math.floor(quantity / maxQuantity * this.barLength), 0)
+      const remainingEmpty = this.barLength - percentageFull
+
+      if (percentageFull) {
+         return this.fullColor + this.character.repeat(percentageFull) + this.emptyColor + this.character.repeat(remainingEmpty)
+      } else {
+         return this.character.repeat(remainingEmpty)
+      }
+   }
+}
+
+module.exports = BarGenerator
