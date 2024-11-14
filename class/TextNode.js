fba44f1121e55f67d583d8c758c5c7dc12a2dfe9
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-14 15:02:55 -0600

    TextNode class

---
+++ class/TextNode.js
@@ -0,0 +1,32 @@
+const { Bot, Vector3 } = require("node-hill-s")
+
+class TextNode {
+	constructor(game, position = new Vector3(0, 0, 0), text) {
+		this.game = game
+		this.bot = null
+		this.position = position
+		this.setText(text)
+	}
+	enableBot() {
+		if (this.bot) return false
+		this.bot = new Bot("")
+		this.bot.position = new Vector3().fromVector(this.position)
+		this.bot.scale = new Vector3(0, 0, 0)
+		this.game.newBot(this.bot)
+	}
+	disableBot() {
+		if (!this.bot) return false
+		this.bot.destroy()
+		this.bot = null
+	}
+	setText(text) {
+		if (text) {
+			this.enableBot()
+			this.bot.setSpeech(text)
+		} else {
+			this.disableBot()
+		}
+	}
+}
+
+module.exports = TextNode
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const BarGenerator = require("./class/BarGenerator.js")
 const Timer = require("./class/Timer.js")
+const TextNode = require("./class/TextNode.js")
 
 module.exports = {
 	BarGenerator,
 	Timer,
+	TextNode,
 	math: require("./math/index.js")
 }
