3545f6a357f1da15b890658f3fdbbcda755b2f1b
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-14 22:08:11 -0600

    Correct syntax error in shuffleArray

--- math/index.js
+++ math/index.js
@@ -3,7 +3,7 @@ function IntFromInterval(min, max) {
 }
 function shuffleArray(array) {
 	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1))
+		const j = Math.floor(Math.random() * (i + 1));
 		[array[i], array[j]] = [array[j], array[i]]
 	}
 }
