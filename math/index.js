f6b649adaf986b605ab614b59a1de05f774ba434
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-11-14 21:20:03 -0600

    Add utility functions for arrays

--- math/index.js
+++ math/index.js
@@ -1,7 +1,21 @@
+function IntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		[array[i], array[j]] = [array[j], array[i]]
+	}
+}
+
+function pickItemInArray(array) {
+	return array[IntFromInterval(0, array.length - 1)]
+}
+
 module.exports = {
 	random: {
-		IntFromInterval: (min, max) => {
-			return Math.floor(Math.random() * (max - min + 1) + min)
-		}
+		shuffleArray,
+		IntFromInterval,
+		pickItemInArray
 	}
 }
