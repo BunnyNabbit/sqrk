083c6c1b6e80b88665d41532be5f0b9f5a9e0940
BunnyNabbit <60326111+BunnyNabbit@users.noreply.github.com>
2024-12-21 10:09:48 -0600

    Enhance Timer class with detailed JSDoc comments for better documentation

--- class/Timer.js
+++ class/Timer.js
@@ -1,6 +1,14 @@
 const EventEmitter = require("events").EventEmitter
 
+/**
+ * Timer class that extends EventEmitter to provide timing functionality.
+ * 
+ * @extends EventEmitter
+ */
 class Timer extends EventEmitter {
+	/**
+	 * Creates an instance of Timer.
+	 */
 	constructor() {
 		super()
 		this.timeStart = Date.now()
@@ -9,11 +17,19 @@ class Timer extends EventEmitter {
 		this.timeout = null
 	}
 
+	/**
+	 * Gets the remaining time in milliseconds.
+	 * 
+	 * @returns {number} The remaining time in milliseconds.
+	 */
 	get timeLeft() {
 		if (!this.active) return 0
 		return Math.max(this.timeEnd - Date.now(), 0)
 	}
 
+	/**
+	 * Updates the timer, setting a new timeout and emitting the "update" event.
+	 */
 	update() {
 		this.clear()
 		this.active = true
@@ -24,17 +40,30 @@ class Timer extends EventEmitter {
 		this.emit("update")
 	}
 
+	/**
+	 * Clears the current timeout and deactivates the timer.
+	 */
 	clear() {
 		this.active = false
 		clearTimeout(this.timeout)
 	}
 
+	/**
+	 * Extends the timer by a specified number of milliseconds.
+	 * 
+	 * @param {number} ms - The number of milliseconds to extend the timer by.
+	 */
 	extend(ms) {
 		this.timeEnd += ms
 		this.update(ms)
 		this.emit("extend", ms)
 	}
 
+	/**
+	 * Sets the timer to end at a specified time.
+	 * 
+	 * @param {number} timeEnd - The time in milliseconds when the timer should end.
+	 */
 	set(timeEnd) {
 		this.timeStart = Date.now()
 		this.timeEnd = this.timeStart + timeEnd
